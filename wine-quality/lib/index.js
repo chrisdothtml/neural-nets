7bfa949ccbc065d5b60646a57b77298d3be6eaec
Chris Deacy <chrisdothtml@gmail.com>
2018-07-20 09:58:13 -0500

    transforming data and building model

--- wine-quality/lib/index.js
+++ wine-quality/lib/index.js
@@ -1,22 +1,72 @@
+require('@tensorflow/tfjs-node')
+
 const csvtojson = require('csvtojson')
+const fs = require('pfs')
+const tf = require('@tensorflow/tfjs')
 const { iife } = require('../../utils.js')
 
-function getData (filepaths) {
-  return Promise.all(
-    filepaths.map(filepath => csvtojson().fromFile(filepath))
+async function getData (filepaths) {
+  const csvOpts = { checkType: true, delimiter: ';' }
+  const wines = await Promise.all(
+    filepaths.map(filepath => csvtojson(csvOpts).fromFile(filepath))
   )
     .then(datasets => {
+      // combine all arrays of data
       return datasets.reduce((result, dataset) => {
         return result.concat(dataset)
       }, [])
     })
+
+  return {
+    // removes output property and converts to array of values
+    inputs: wines.map(item => {
+      let result = { ...item }
+      delete result.quality
+      return Object.values(result)
+    }),
+    // converts to array of values of output property
+    outputs: wines.map(item => item.quality)
+  }
+}
+
+function createModel (data) {
+  const model = tf.sequential()
+
+  // hidden layer
+  model.add(tf.layers.dense({
+    activation: 'sigmoid',
+    inputDim: Object.keys(data.inputs[0]).length,
+    units: 32
+  }))
+
+  // output layer
+  model.add(tf.layers.dense({
+    activation: 'linear',
+    units: 1
+  }))
+
+  model.compile({
+    loss: 'meanSquaredError',
+    optimizer: tf.train.sgd(0.2)
+  })
+
+  return model
 }
 
 iife(async () => {
-  const wines = await getData([
+  const data = await getData([
     './data/winequality-red.csv',
     './data/winequality-white.csv'
   ])
+  const model = createModel(data)
+  const results = await model.fit(
+    tf.tensor2d(data.inputs),
+    tf.tensor1d(data.outputs),
+    {
+      epochs: 10,
+      validationSplit: 0.2
+    }
+  )
 
-  console.log(wines.length)
+  console.log(results)
 })
